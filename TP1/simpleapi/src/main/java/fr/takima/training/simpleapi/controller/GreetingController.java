f87ae536cdf97cff7e0bebf180ed7a03cedaf0ff
Oscar Hunaut <oscar.hunaut@epfedu.fr>
2026-06-03 16:19:00 +0200

    Updated CI/CD and deployment

--- TP1/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ TP1/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -8,11 +8,11 @@ import java.util.concurrent.atomic.AtomicLong;
 @CrossOrigin
 public class GreetingController {
 
-    private static final String TEMPLATE = "Hello, %s!";
+    private static final String TEMPLATE = "Bonjour, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Le Monde") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 
